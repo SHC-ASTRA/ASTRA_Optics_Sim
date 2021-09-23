7ce9b82cbfc405b6afea496b1f632c4b0edcc7de
Thomas Bennett <trb0023@uah.edu>
2021-09-23 17:04:41 -0500

    Create rayArcIntersect.m

---
+++ rayArcIntersect.m
@@ -0,0 +1,9 @@
+function [outputArg1,outputArg2] = rayArcIntersect(RayStart,RayDir,arcCenter,arcRadius,arcAngleRange)
+
+%line equation: y = dy*(x-x0) + y0 or x = dx*(y-y0) + x0
+%circle equation r^2 = x^2 + y^2
+
+
+
+end
+
